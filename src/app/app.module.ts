ac363cc9c8f4668bb9a7f3bcffc570dde9572135
Dora Posta <dora_posta@epam.com>
2024-05-22 13:15:58 +0200

    finished data binding assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html'
+})
+export class InputComponent {
+  userName = ''
+
+  onDeleteUserName() {
+    this.userName = ''
+  }
+
+}
